c4081a56bd4b33c4ae88917cd9cd4c0bfb4cf19a
Kevin Lindsey <kevin@kevlindev.com>
2013-08-19 19:14:48 -0500

    Update imports

--- examples/repeat-iterator.js
+++ examples/repeat-iterator.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-var RangeIterator = require('../lib/RangeIterator'),
-    RepeatIterator = require('../lib/RepeatIterator');
+var RangeIterator = require('kld-array-iterators').RangeIterator,
+    RepeatIterator = require('kld-array-iterators').RepeatIterator;
 
 var iter = new RepeatIterator(
     new RangeIterator(0, 10),
