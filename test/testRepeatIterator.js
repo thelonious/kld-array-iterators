28b4c495c4e6ec6a819123bd593c868f1b08fa2b
Kevin Lindsey <kevin@kevlindev.com>
2013-08-20 14:02:22 -0500

    Fix "next" to handle zero case

---
+++ test/testRepeatIterator.js
@@ -0,0 +1,72 @@
+var Iterator = require('../lib/Iterator'),
+    RepeatIterator = require('../lib/RepeatIterator');
+
+exports.testZeroOrOne = function(beforeExit, assert) {
+    var iter = new RepeatIterator(
+        new Iterator(1, 2, 3),
+        0,
+        1
+    );
+
+    assert.equal(true, iter.hasNext());
+    assert.equal(null, iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([1], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([2], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([3], iter.next());
+
+    assert.equal(false, iter.hasNext());
+    assert.equal(null, iter.next());
+};
+
+exports.testOneOrTwo = function(beforeExit, assert) {
+    var iter = new RepeatIterator(
+        new Iterator(1, 2, 3),
+        1,
+        2
+    );
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([1], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([2], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([3], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([1, 1], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([1, 2], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([1, 3], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([2, 1], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([2, 2], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([2, 3], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([3, 1], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([3, 2], iter.next());
+
+    assert.equal(true, iter.hasNext());
+    assert.eql([3, 3], iter.next());
+
+    assert.equal(false, iter.hasNext());
+    assert.equal(null, iter.next());
+};
